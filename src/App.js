88243f0d2aa10cb913b9deae3d01cc8b7c0e55f5
Niko Shin <nshin17@gmail.com>
2023-03-09 17:05:49 -0800

    Imported necessary files.

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from "react";
+import "./style.css";
+import Header from "./components/Header";
+import Main from "./components/Main";
 
 function App() {
+  // State that determines if current round is still going or ended :
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="app--container">
+      <Header />
+      <Main />
     </div>
   );
 }
