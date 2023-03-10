9e5e71aacf139a24be066681db446c5c87158107
Niko Shin <nshin17@gmail.com>
2023-03-10 12:26:30 -0800

    Moved gameLevel state to this component (parent) from Card.js child component for state lifting

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,11 +2,19 @@
 
 import React from "react";
 import Card from "./Card";
+import Timer from "./Timer";
+import Levels from "./Levels";
 
 function Main() {
+  const [gameLevel, setGameLevel] = React.useState(1);
+
   return (
     <>
-      <Card />
+      <div className="main--score_tracker">
+        <Timer />
+        <Levels currentLevel={gameLevel} />
+      </div>
+      <Card level={gameLevel} />
     </>
   );
 }
