c3111c3a4674eadbe859b50cd0a00e7399484ab1
Niko Shin <nshin17@gmail.com>
2023-03-15 18:27:38 -0700

    Changed style attribute from display to opacity so DOM elements are still presnt and clickable by player.

--- src/components/Card.js
+++ src/components/Card.js
@@ -65,8 +65,8 @@ function Card(props) {
             data-uuid={card.uuid}
             src={card.url}
             alt=""
-            style={{ visibility: !card.hidden ? "visible" : "hidden" }}
-            // style={{ visibility: card.hidden ? "visible" : "hidden" }}
+            // style={{ opacity: card.hidden ? 0 : 100 }}
+            style={{ opacity: !card.hidden ? 0 : 100 }}
           />
         </div>
       )
@@ -80,3 +80,5 @@ function Card(props) {
 export default Card;
 
 // Hide all cards by default. If clicked on, display: hidden attribute is removed to show the card.
+
+// Initially, each card displayed on the page has a div parent for each image => After player clicks on an image, the object is returned but without the div parent causing the event handler to not respond properly => Resolve by either dealing with the div parent (removing it altogether) or having the click event return the proper code with parent div's...
